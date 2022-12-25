474f1c1b0fecd1f82bd27505ca89b971653a3535
Liqht <schoolsmarty@hotmail.com>
2022-12-25 01:49:55 -0600

    Added particles and a sound that plays when a player uses the HadesScythe

--- src/main/java/net/liqht/liqhtlab/admin/items/HadesScythe.java
+++ src/main/java/net/liqht/liqhtlab/admin/items/HadesScythe.java
@@ -1,6 +1,8 @@
 package net.liqht.liqhtlab.admin.items;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -42,12 +44,15 @@ public class HadesScythe implements CommandExecutor, Listener {
     public void onUse(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         if (Objects.requireNonNull(player.getInventory().getItemInMainHand().getItemMeta()).getDisplayName().equals(ChatColor.DARK_RED + "Hades")) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 200, 999999999,
+            player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 100, 999999999,
                     true, true, true));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 200, 5,
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100, 5,
                     true, true, true));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 200, 5,
+            player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 100, 5,
                     true, true, true));
+
+            player.spawnParticle(Particle.DRAGON_BREATH, player.getLocation(), 200);
+            player.playSound(player.getLocation(), Sound.BLOCK_END_PORTAL_SPAWN, 1, 1);
         }
     }
 }
