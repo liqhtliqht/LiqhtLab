300fd1a5c08baaca9a007d7b80b7c2cd7c88ac0b
Liqht <schoolsmarty@hotmail.com>
2022-12-25 00:20:54 -0600

    Added a texture pack that will be enabled on join

--- src/main/java/net/liqht/liqhtlab/everyone/listeners/ServerJoinStuff.java
+++ src/main/java/net/liqht/liqhtlab/everyone/listeners/ServerJoinStuff.java
@@ -6,7 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class ServerJoinMessage implements Listener {
+public class ServerJoinStuff implements Listener {
     @EventHandler
     public static void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
@@ -15,6 +15,8 @@ public class ServerJoinMessage implements Listener {
         event.getPlayer().sendMessage(ChatColor.BOLD + "Welcome to the Liqht_'s Lab!\n\n");
         event.getPlayer().sendMessage("We use mods on this server that will enhance your community experience. For more into, go here\n\n");
         event.getPlayer().sendMessage(ChatColor.UNDERLINE + "https://docs.google.com/document/d/1mKJiI4fFXdLH6kIIYt2BeQe27PiKwnlGMbhEWElO6pE/edit?usp=sharing\n");
+
+        event.getPlayer().setResourcePack("https://download.mc-packs.net/pack/6c46e756bda7d12c0844cd49519870ad48b6670a.zip");
 
     }
 }
